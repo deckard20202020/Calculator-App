b4bc889ac8d8a2d18511278b543966cd9bf46ee8
Matt Sinnwell <mattdsinnwell@gmail.com>
2023-10-27 17:44:04 -0500

    Calculator App for Coursera React Basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import "./App.css";
 
 function App() {
+  const inputRef = useRef(null);
+  const [result, setResult] = useState(0);
+
+  function plus(e) {
+    e.preventDefault();
+    const inputValue = Number(inputRef.current.value);
+    setResult(result + inputValue);
+    resetInput();
+  }
+
+  function minus(e) {
+    e.preventDefault();
+    const inputValue = Number(inputRef.current.value);
+    setResult(result - inputValue);
+    resetInput();
+  }
+
+  function times(e) {
+    e.preventDefault();
+    const inputValue = Number(inputRef.current.value);
+    setResult(result * inputValue);
+    resetInput();
+  }
+
+  function divide(e) {
+    e.preventDefault();
+    const inputValue = Number(inputRef.current.value);
+    
+    if (inputValue === 0) {
+      alert("Cannot divide by zero!");
+      return;
+    }
+    setResult(result / inputValue);
+    resetInput();
+  }
+
+  function resetInput() {
+    inputRef.current.value = "";
+  }
+
+  function resetResult(e) {
+    e.preventDefault();
+    setResult(0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Simplest Working Calculator</h1>
+      </div>
+      <form>
+        <p>{result}</p>
+
+        <input
+          pattern="[0-9]"
+          ref={inputRef}
+          type="number"
+          placeholder="Type a number"
+        />
+
+        <button type="button" onClick={plus}>add</button>
+        <button type="button" onClick={minus}>subtract</button>
+        <button type="button" onClick={times}>multiply</button>
+        <button type="button" onClick={divide}>divide</button>
+        <button type="button" onClick={resetInput}>reset input</button>
+        <button type="button" onClick={resetResult}>reset result</button>
+      </form>
     </div>
   );
 }
 
 export default App;
+
